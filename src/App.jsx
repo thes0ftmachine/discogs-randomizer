5439b91e3bbf505031b1e4803ce288a4191d01dd
thes0ftmachine <46765526+thes0ftmachine@users.noreply.github.com>
2026-08-02 20:38:52 -0700

    Update border opacity for styleChip

--- src/App.jsx
+++ src/App.jsx
@@ -1630,7 +1630,7 @@ const styles = {
     fontSize: 12,
     fontWeight: 600,
     background: "#6FA987",
-    border: `1px solid ${PALETTE.border}55`,
+    border: `1px solid ${PALETTE.border}95`,
     padding: "5px 10px",
     borderRadius: 999,
     color: PALETTE.primary,
